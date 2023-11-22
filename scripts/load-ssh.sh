2b15287ef586aa8f605b1d84737908b23a6f440b
Bipin Maharjan <21078512+iambpn@users.noreply.github.com>
2023-11-22 11:22:56 +0545

    Update load-ssh.sh

--- scripts/load-ssh.sh
+++ scripts/load-ssh.sh
@@ -5,7 +5,7 @@ set -e
 SSH_PATH="../.ssh/*"
 echo "Decrypting File"
 ansible-vault decrypt $SSH_PATH
-echo "Moving File to '$HOME'/.ssh/"
-mv $SSH_PATH "$HOME/.ssh/" -i
-git reset --hard &> /dev/null
+echo "Moving File to $HOME/.ssh/"
+mv -i $SSH_PATH "$HOME/.ssh/"
+git reset --hard &> /dev/null # reset git
 echo "Decrypting completed"
